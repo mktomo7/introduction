a023b71f1ae1073e969597971e9a6963a3ddbf79
麝香 <mktomo7@gmail.com>
2019-05-09 19:13:57 +0900

    no message

--- works/counter.js
+++ works/counter.js
@@ -4,3 +4,9 @@ const countUp = () => {
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: ' + count
 }
+const resset = () => {
+    count++
+  const countElement = document.querySelector('#count')
+  count = 0
+  countElement.innerText = '回数: ' + count
+}
